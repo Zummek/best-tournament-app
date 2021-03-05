e7f07dc3521a541fd860f170442714ace662a696
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-05 17:09:05 +0100

    Init commit
    Added:
    - files structure
    - docker for database
    - connection to database
    - readme

---
+++ server/database/index.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+export const connectDatabase = () => {
+  const uri = `mongodb://${process.env.DB_DEV_HOST}:${process.env.DB_DEV_PORT}/${process.env.DB_DEV_DATABASE}?authSource=admin`;
+
+  mongoose
+    .connect(uri, {
+      user: process.env.DB_DEV_USERNAME,
+      pass: process.env.DB_DEV_PASSWORD,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connection succesful');
+    })
+    .catch((error) => {
+      console.log('DB connection error', error);
+    });
+};
