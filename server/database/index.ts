562b2c135b4f7e2ad01bff9f9892c64a53546d7d
eastemployeer <kamilzab27@gmail.com>
2021-03-06 19:30:57 +0100

    Added mongoDB connection

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,14 +1,19 @@
-// import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-// mongoose
-//   .connect(process.env.DATABASE!, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("DB connection succesful");
-//   }).catch(() => {
-//     console.log("DB connection error: ");
-//   });
+export const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_LOCAL!, {
+      user: process.env.DB_DEV_USERNAME,
+      pass: process.env.DB_DEV_PASSWORD,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connection succesful');
+    })
+    .catch(() => {
+      console.log('DB connection error: ');
+    });
+};
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
-
+import { connectDatabase } from './database/index';
 
 // eslint-disable-next-line import/first
-import app from "./app";
+import app from './app';
 
 const port = process.env.PORT || 3000;
+
+connectDatabase();
 
 // eslint-disable-next-line no-unused-vars
 const server = app.listen(port, () => {
