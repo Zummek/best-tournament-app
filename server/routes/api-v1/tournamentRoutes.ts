1f6399c749315d1e1e3a8a8db1757f10f85e5fd4
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-09 00:37:47 +0100

    Change conception for controllers

--- server/routes/api-v1/tournamentRoutes.ts
+++ server/routes/api-v1/tournamentRoutes.ts
@@ -1,14 +1,8 @@
 import express from 'express';
-import createTournament from '../../controllers/tournament/createTournament';
+import tournamentController from '../../controllers/tournament/';
 
 const router = express.Router();
 
-router.post('', (req, res, next) => {
-  const { name, owner, participants, matches } = req.body;
-
-  createTournament({ name, owner, participants, matches });
-
-  res.status(204).end();
-})
+router.post('', tournamentController.create);
 
 export default router;
