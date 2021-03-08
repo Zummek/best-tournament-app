339e0517f7fe08c921024b38cf0db544449c7ce3
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-08 20:03:10 +0100

    Add create tournament endpoint [WIP] waiting for db model

---
+++ server/controllers/tournament/createTournament.ts
@@ -0,0 +1,7 @@
+import ITournament from '../../../shared/types/Tournament';
+
+const createTournament = async (newTournament: ITournament) => {
+  // todo: waiting for tournament database model
+}
+
+export default createTournament;
