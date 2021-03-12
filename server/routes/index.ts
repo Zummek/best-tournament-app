fa070e44c7256cb2a4946287b43db31ac0683bbb
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-12 14:30:15 +0100

    Remove writeResponse helper, update routes this project is too simple to use writeResponse helper

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './api-v1/userRoutes';
 import organizationRouter from './api-v1/organizationRoutes';
+import tournamentRoutes from './api-v1/tournamentRoutes';
 
 const indexRouter = express.Router();
 
 indexRouter.use('/v1/users/', userRouter);
 indexRouter.use('/v1/organization/', organizationRouter);
+indexRouter.use('/v1/tournament', tournamentRoutes);
 
 export default indexRouter;
