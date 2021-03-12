94d911c84d3274f0201cb4318b7ea17712a59059
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-13 00:35:22 +0100

    [CU-f92zha] Create login view and NoApp layout (#5)
    * Add NoAppLayout and Login view
    View is ready to connect with backend.
    After catch session edit `Router.beforeEach` condition
    
    * Update layout and view to class component

---
+++ web-app/src/layouts/NoAppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container class="page-container">
+      <router-view @change-tab="value => (tab = value)" />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class NonAppLayout extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  background-color: $primary;
+}
+</style>
--- web-app/src/router/routes.ts
+++ web-app/src/router/routes.ts
@@ -2,19 +2,34 @@ import { RouteConfig } from 'vue-router';
 
 const routes: RouteConfig[] = [
   {
-    path: '/',
+    path: '/app',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+        meta: { public: false },
+      },
+    ],
+  },
+  {
+    path: '/',
+    component: () => import('layouts/NoAppLayout.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('pages/Login.vue'),
+        meta: { public: true },
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '*',
-    component: () => import('pages/Error404.vue')
-  }
+    component: () => import('pages/Error404.vue'),
+  },
 ];
 
 export default routes;
