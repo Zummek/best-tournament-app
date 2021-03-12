337ac2cf1b89ebca815e1b71d6025f0d84d9148f
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-12 14:05:39 +0100

    Update routes

--- server/routes/api-v1/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import tournamentRoutes from './tournamentRoutes';
-
-const router = express.Router();
-
-router.use('/tournament', tournamentRoutes);
-
-export default router;
