26ef5db7832ba531d946c702678e76d0616d0ea3
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-13 01:47:30 +0100

    Update tournament router - add '/' to post endpoint

--- server/routes/api-v1/tournamentRoutes.ts
+++ server/routes/api-v1/tournamentRoutes.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import tournamentController from '../../controllers/tournament';
+import express from "express";
+import tournamentController from "../../controllers/tournament";
 
 const tournamentRouter = express.Router();
 
-tournamentRouter.post('', tournamentController.create);
+tournamentRouter.post("/", tournamentController.create);
 
 export default tournamentRouter;
