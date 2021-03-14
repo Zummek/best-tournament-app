ed6d533fb9fd5ca8e23bbab5354e806101238ae8
Buglik <kubula243243@gmail.com>
2021-03-14 17:44:31 +0100

    basic login layout added

--- web-app/src/pages/Login.vue
+++ web-app/src/pages/Login.vue
@@ -1,40 +1,47 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="row full-width justify-center">
-      <q-card class="col-sm-6 col-md-4 col-lg-3">
-        <q-card-section class="row justify-center">
-          <div class="col-9 text-center">
-            <q-icon name="emoji_events" size="4.4em" />
-            <div class="text-h6">
-              Welcome to <br />
-              best tournament application!
+  <q-page class="flex flex-center" style="background: white">
+    <div class="row full-width justify-center">    
+        <q-card class="col-sm-0 col-md-3 col-lg-3">
+          <q-img
+            src="https://placeimg.com/500/300/nature"
+            :ratio="1"
+            height="100%"
+            />
+        </q-card>
+        <q-card class="col-sm-7 col-md-4 col-lg-4">
+          <q-card-section class="row justify-center">
+            <div class="col-9 text-center">
+              <q-icon name="emoji_events" size="4.4em" />
+              <div class="text-h6">
+                Welcome to <br />
+                best tournament application!
+              </div>
+              <div class="text-subtitle2" style="margin-top: 20px">
+                Sign in via Microsoft Office account and enjoy company fun
+              </div>
             </div>
-            <div class="text-subtitle2" style="margin-top: 20px">
-              Sign in via Microsoft Office account and enjoy company fun
-            </div>
-          </div>
-        </q-card-section>
-        <q-card-section class="row justify-center">
-          <q-btn
+          </q-card-section>
+          <q-card-section class="row justify-center">
+            <q-btn
             color="primary"
             icon="mail"
             label="Login with Microsoft"
             @click="loginWithMS"
-          />
-        </q-card-section>
-        <q-card-section class="row justify-center authors text-weight-light">
+            />
+          </q-card-section>
+          <q-card-section class="row justify-center authors text-weight-light">
           Authors: <br />
           Adam Jędryka, Przemysław Rychter <br />
           Kuba Pawleniak, Kamil Zaborowski
-        </q-card-section>
-      </q-card>
+          </q-card-section>
+        </q-card>
+      
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-
 @Component
 export default class Login extends Vue {
   loginWithMS() {
