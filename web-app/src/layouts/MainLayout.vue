445c302b35959787ca78ac7d1231e97097cfcaef
Buglik <kubula243243@gmail.com>
2021-03-16 21:41:22 +0100

    better looking mobile version

--- web-app/src/layouts/MainLayout.vue
+++ web-app/src/layouts/MainLayout.vue
@@ -2,21 +2,16 @@
   <q-layout view="hHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          class="lt-md"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
+        <q-toolbar-title class="text-center lt-sm">
           <q-icon name="emoji_events" />
           Best tournament app
         </q-toolbar-title>
-        <div class="row gt-sm">
+        <q-toolbar-title class="gt-xs">
+          <q-icon name="emoji_events" />
+          Best tournament app
+        </q-toolbar-title>
+
+        <div class="row gt-xs">
           <!-- <q-item-label header>Options</q-item-label> -->
           <MenuElement
             v-for="link in menuLinks"
@@ -67,53 +62,65 @@
             </q-btn>
           </div>
         </div>
+
+        <div class="column lt-sm">
+          <div>
+            <!-- Test -->
+            <q-btn round class="q-ma-sm">
+              <q-avatar size="40px">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+
+              <q-menu anchor="bottom right" self="top right">
+                <div class="row no-wrap q-pa-md justify-center">
+                  <div class="column items-center">
+                    <q-avatar size="72px">
+                      <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                    </q-avatar>
+
+                    <div class="text-subtitle1 q-mt-md">Adam Kowalski</div>
+                    <div class="text-weight-light q-mb-md">
+                      mailkowalskiego@la.pl
+                    </div>
+
+                    <q-btn
+                      color="primary"
+                      label="Logout"
+                      push
+                      size="sm"
+                      v-close-popup
+                    />
+                  </div>
+                </div>
+                <q-separator horizontal class="q-mx-xs" />
+                <div class="row  q-pa-md">
+                  <div class="column items-center">
+                    <div class="text-h6 q-mb-md">Settings</div>
+                    It's me mario here. I will provide some setting later.
+                  </div>
+                </div>
+              </q-menu>
+            </q-btn>
+          </div>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      :width="250"
-      :height="500"
-      :breakpoint="1000" 
-      bordered
-      elevated
-      content-class="bg-grey-1"
-    >
-      <q-scroll-area
-        style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd"
+    <q-footer bordered class="bg-blue text-primary lt-sm">
+      <q-tabs
+        no-caps
+        active-color="primary"
+        indicator-color="transparent"
+        class="text-grey"
+        v-model="tab"
       >
-        <q-list>
-          <MenuElement
-            v-for="link in menuLinks"
-            :key="link.title"
-            v-bind="link"
-          />
-        </q-list>
-      </q-scroll-area>
-
-      <!-- TODO logo organizacji -->
-      <q-img
-        class="absolute-top"
-        src="https://cdn.quasar.dev/img/material.png"
-        style="height: 150px"
-      >
-        <div class="absolute-bottom bg-transparent">
-          <q-avatar size="75px" class="q-mb-sm">
-            <!-- TODO Avatar użytkownika -->
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-          <div class="text-weight-bold">Adam Kowalski</div>
-          <div>mailkowalskiego@la.pl</div>
-        </div>
-        <q-btn
-          color="primary"
-          label="Logout"
-          push
-          size="sm"
-          class="absolute-top-right q-mt-sm q-mr-sm"
+        <q-tab
+          v-for="link in menuLinks"
+          :label="link.title"
+          :name="link.title"
         />
-      </q-img>
-    </q-drawer>
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -126,8 +133,11 @@ import MenuElement from '../components/MenuElement.vue';
 
 const linksData = [
   {
-    title: 'Create new tournament',
-    icon: 'add',
+    title: 'Tab1',
+    link: '#',
+  },
+  {
+    title: 'Tab2',
     link: '#',
   },
 ];
