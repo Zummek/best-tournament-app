e544fc3be0184be4afe3939596d2addedfc58559
Buglik <kubula243243@gmail.com>
2021-03-18 12:24:14 +0100

    better version simplified some of the code, solved a problem with title overflow, changed tabs names (also set equal width at mobile view), deleted HeaderMenuElement, removed some trash as well

--- web-app/src/layouts/MainLayout.vue
+++ web-app/src/layouts/MainLayout.vue
@@ -2,68 +2,70 @@
   <q-layout view="hHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <div class="row col-3 gt-xs">
-          <HeaderMenuElement
+        <div class="row col-4 gt-xs">
+          <q-item
             v-for="link in menuLinks"
-            :key="link.title"
-            v-bind="link"
-          />
+            clickable
+            tag="a"
+            target="_blank"
+            :href="link.link"
+            :key="link.label"
+          >
+            <q-item-section>
+              <q-item-label>{{ link.label }}</q-item-label>
+            </q-item-section>
+          </q-item>
         </div>
 
-        <q-toolbar-title class="lt-sm">
+        <q-toolbar-title
+          :class="$q.screen.gt.xs ? 'text-center gt-xs' : ''"
+          style="overflow:visible"
+        >
           <q-icon name="emoji_events" />
           Best tournament app
         </q-toolbar-title>
-        <q-toolbar-title class="gt-xs text-center">
-          <q-icon name="emoji_events" />
-          Best tournament app
-        </q-toolbar-title>
 
-        <div class="row col-3 gt-xs justify-end">
-          <div>
-            <!-- Test -->
-            <q-btn round class="q-ma-sm ">
-              <q-avatar size="50px">
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
+        <div class="row col-4 gt-xs justify-end">
+          <q-btn round class="q-ma-sm ">
+            <q-avatar size="50px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
 
-              <q-menu anchor="bottom right" self="top right">
-                <div class="row no-wrap q-pa-md">
-                  <div class="column" style="flex: 2">
-                    <div class="text-h6 q-mb-md">Settings</div>
-                    It's me mario here.<br />
-                    I will provide some setting here later.
-                  </div>
-
-                  <q-separator vertical inset class="q-mx-lg" />
-
-                  <div class="column items-center" style="flex: 1">
-                    <q-avatar size="72px">
-                      <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-                    </q-avatar>
-
-                    <div class="text-subtitle1 q-mt-md">Adam Kowalski</div>
-                    <div class="text-weight-light q-mb-md">
-                      mailkowalskiego@la.pl
-                    </div>
-
-                    <q-btn
-                      color="primary"
-                      label="Logout"
-                      push
-                      size="sm"
-                      v-close-popup
-                    />
-                  </div>
+            <q-menu anchor="bottom right" self="top right">
+              <div class="row no-wrap q-pa-md">
+                <div class="column" style="flex: 2">
+                  <div class="text-h6 q-mb-md">Settings</div>
+                  It's me mario here.<br />
+                  I will provide some setting here later.
                 </div>
-              </q-menu>
-            </q-btn>
-          </div>
+
+                <q-separator vertical inset class="q-mx-lg" />
+
+                <div class="column items-center" style="flex: 1">
+                  <q-avatar size="72px">
+                    <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                  </q-avatar>
+
+                  <div class="text-subtitle1 q-mt-md">Adam Kowalski</div>
+                  <div class="text-weight-light q-mb-md">
+                    mailkowalskiego@la.pl
+                  </div>
+
+                  <q-btn
+                    color="primary"
+                    label="Logout"
+                    push
+                    size="sm"
+                    v-close-popup
+                  />
+                </div>
+              </div>
+            </q-menu>
+          </q-btn>
         </div>
 
         <div class="column lt-sm">
           <div>
-            <!-- Test -->
             <q-btn round class="q-ma-sm">
               <q-avatar size="40px">
                 <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
@@ -107,13 +109,14 @@
     <q-footer bordered class="bg-primary text-primary lt-sm">
       <q-tabs
         v-model="tab"
+        align="justify"
         indicator-color="transparent"
         active-color="white"
         class="bg-primary text-grey-5 shadow-2"
       >
-        <q-tab label="Tab1" name="Tab1" />
+        <q-tab label="News" name="News" class="row col-6" />
         <q-separator vertical class="q-mx-xs" />
-        <q-tab label="Tab2" name="Tab2" />
+        <q-tab label="Tournaments" name="Tournaments" class="row col-6" />
       </q-tabs>
     </q-footer>
 
@@ -124,26 +127,21 @@
 </template>
 
 <script lang="ts">
-import HeaderMenuElement from '../components/HeaderMenuElement.vue';
-
 const linksData = [
   {
-    title: 'Tab1',
+    label: 'News',
     link: '#',
   },
   {
-    title: 'Tab2',
+    label: 'Tournaments',
     link: '#',
   },
 ];
 
 import { Vue, Component } from 'vue-property-decorator';
 
-@Component({
-  components: { HeaderMenuElement },
-})
+@Component
 export default class MainLayout extends Vue {
-  leftDrawerOpen = false;
   menuLinks = linksData;
 }
 </script>
