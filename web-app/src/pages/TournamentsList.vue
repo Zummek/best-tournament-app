95b18e1ba53e47538d998b4cd60e3571538f6df9
Buglik <kubula243243@gmail.com>
2021-03-19 21:18:03 +0100

    improved mobile version deleted pagination, nested list items in btn, added green/orange status color, added sticky addBtn, commented Participants section,

--- web-app/src/pages/TournamentsList.vue
+++ web-app/src/pages/TournamentsList.vue
@@ -8,7 +8,10 @@
       :columns="columns"
       row-key="name"
       :filter="filter"
-      hide-header
+      virtual-scroll
+      :pagination="pagination"
+      :rows-per-page-options="[0]"
+      :expanded.sync="expanded"
     >
       <template v-slot:top-right>
         <q-input
@@ -25,42 +28,53 @@
       </template>
 
       <template v-slot:item="props">
-        <div class="q-pa-xs col-12 ">
-          <q-card class="row justify-between">
-            <q-card-section
-              class="col-6 overflow-hidden"
-              style="text-align:left"
-            >
-              <div class="text-grey-14">
-                Name:
-              </div>
-              <strong>{{ props.row.name }}</strong>
-            </q-card-section>
-            <q-card-section class="col-6" style="text-align:right">
-              {{ props.row.status }}
-            </q-card-section>
-            <q-separator vertical class="q-mx-xs" />
-            <q-card-section class="col-12">
-              <div class="text-grey-14 ">
-                Participants:
-              </div>
-              <q-chip>
-                <q-avatar>
+        <div class="row q-pa-xs col-12">
+          <q-btn no-caps flat padding="none" class="col-12">
+            <q-card class="row col-12">
+              <q-card-section
+                class="col-6 overflow-hidden"
+                style="text-align:left"
+              >
+                <div class="text-grey-14">
+                  Name:
+                </div>
+                <strong>{{ props.row.name }}</strong>
+              </q-card-section>
+              <q-card-section
+                class="col-6"
+                style="text-align:right"
+                :style="
+                  props.row.status === 'In progress'
+                    ? 'color: orange'
+                    : 'color: green'
+                "
+              >
+                {{ props.row.status }}
+              </q-card-section>
+              <!-- <q-separator vertical class="q-mx-xs" />
+              <q-card-section class="col-12">
+                <div class="text-grey-14 " style="text-align:left">
+                  Participants:
+                </div>
+                
+                <q-chip size="1em">
+                  <q-avatar>
+                    <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                  </q-avatar>
+                  Organizer
+                </q-chip>
+                <q-avatar size="2em" v-for="n in 10" :key="n" class="q-ml-xs">
                   <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
                 </q-avatar>
-                JA
-              </q-chip>
-              <q-avatar
-                v-for="n in 15"
-                :key="n"
-                icon="img:https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
-              >
-              </q-avatar>
-            </q-card-section>
-          </q-card>
+              </q-card-section> -->
+            </q-card>
+          </q-btn>
         </div>
       </template>
     </q-table>
+    <q-page-sticky position="bottom-right" :offset="[36, 18]">
+      <q-btn round color="primary" icon="add" />
+    </q-page-sticky>
   </div>
 </template>
 <script lang="ts">
@@ -69,6 +83,10 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component
 export default class TournamentsList extends Vue {
   private filter = '';
+  private expanded = '';
+  private pagination = {
+    rowsPerPage: 0,
+  };
   private columns = [
     {
       name: 'desc',
@@ -94,14 +112,34 @@ export default class TournamentsList extends Vue {
   ];
   private data = [
     {
-      name: 'Ala ma kota i kocha tego kota i wgl go uwielbia',
-      status: 'in progresss..',
+      name: 'Work',
+      status: 'In progress',
       participants: 'You and I',
     },
     {
       name: 'Tourn2',
-      status: 'Finished.',
+      status: 'Finished',
       participants: 'You and I',
+    },
+    {
+      name: 'Tourn3',
+      status: 'Finished',
+      participants: 'No one',
+    },
+    {
+      name: 'Tourn4',
+      status: 'Finished',
+      participants: 'No one',
+    },
+    {
+      name: 'Tourn5',
+      status: 'Finished',
+      participants: 'No one',
+    },
+    {
+      name: 'Tourn6',
+      status: 'Finished',
+      participants: 'No one',
     },
   ];
 }
