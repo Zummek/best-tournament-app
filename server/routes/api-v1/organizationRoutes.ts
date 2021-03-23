f03b6b2b303043f1752785bdce2a82697375e4e9
eastemployeer <kamilzab27@gmail.com>
2021-03-23 19:45:47 +0100

    Added missing export

--- server/routes/api-v1/organizationRoutes.ts
+++ server/routes/api-v1/organizationRoutes.ts
@@ -4,3 +4,5 @@ import * as authController from '../../controllers/authController';
 const organizationRouter = express.Router();
 
 organizationRouter.get('/logo', authController.getAzureADApplicationLogo);
+
+export default organizationRouter;
