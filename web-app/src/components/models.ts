1a09a8cbd3203b62e2c0a8996787f49bae53d7b6
eastemployeer <kamilzab27@gmail.com>
2021-03-23 15:48:23 +0100

    Fixed tournament structure

--- web-app/src/components/models.ts
+++ web-app/src/components/models.ts
@@ -6,3 +6,20 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+export interface IPagination {
+  rowsPerPage: number;
+}
+export interface IColumns {
+  name?: string;
+  required?: boolean;
+  label?: string;
+  algin?: string;
+  field?: string;
+  sortable?: boolean;
+}
+
+export interface IData {
+  name?: string;
+  status?: string;
+  participants?: string;
+}
---
+++ web-app/src/components/tournament/GridListMobile.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="q-pa-md lt-sm">
+    <q-table
+      grid
+      title="Tournaments"
+      :data="data"
+      :columns="columns"
+      row-key="name"
+      :filter="filter"
+      virtual-scroll
+      :pagination="pagination"
+      :rows-per-page-options="[0]"
+      :expanded.sync="expanded"
+    >
+      <template v-slot:top-right>
+        <q-input
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="Search"
+          style="width:150px"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+      <template v-slot:item="props">
+        <div class="row q-pa-xs col-12">
+          <q-btn no-caps flat padding="none" class="col-12">
+            <q-card class="row col-12">
+              <q-card-section
+                class="col-6 overflow-hidden"
+                style="text-align:left"
+              >
+                <div class="text-grey-14">
+                  Name:
+                </div>
+                <strong>{{ props.row.name }}</strong>
+              </q-card-section>
+              <q-card-section
+                class="col-6"
+                style="text-align:right"
+                :style="
+                  props.row.status === 'In progress'
+                    ? 'color: orange'
+                    : 'color: green'
+                "
+              >
+                {{ props.row.status }}
+              </q-card-section>
+            </q-card>
+          </q-btn>
+        </div>
+      </template>
+    </q-table>
+    <q-page-sticky position="bottom-right" :offset="[36, 18]">
+      <q-btn round color="primary" icon="add" />
+    </q-page-sticky>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { IPagination, IData, IColumns } from '../models';
+
+@Component
+export default class GridListMobile extends Vue {
+  @Prop({ type: String, default: () => '' }) filter!: string;
+  @Prop({ type: String, default: () => '' }) readonly expanded!: string;
+  @Prop({ type: Array, required: true }) readonly columns!: IColumns;
+  @Prop({ type: Array, default: () => [] }) readonly data!: IData[];
+  @Prop({ type: Object, required: true }) readonly pagination!: IPagination;
+}
+</script>
