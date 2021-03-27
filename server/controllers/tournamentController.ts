64bfde1ef244d76bd0286f973130e77a3d351c7e
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-27 09:45:07 +0100

    temporary as template

---
+++ server/controllers/tournamentController.ts
@@ -0,0 +1,11 @@
+import catchAsync from '../utils/catchAsync';
+
+const createTournament = catchAsync(
+  (re// tournamnet class.create
+    res.status(204).end();
+  },
+);
+
+export default {
+  create: createTournament
+};
