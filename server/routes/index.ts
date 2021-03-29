560b0a8a55d0d8b40bb5a152cf127264ad02a27e
Adam Jędryka <adam.jedryka98@gmail.com>
2021-03-29 23:53:15 +0200

    Change files names

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import userRouter from './api-v1/userRoutes';
-import organizationRouter from './api-v1/organizationRoutes';
-import tournamentRoutes from './api-v1/tournamentRoutes';
+import userRouter from './api-v1/userRouter';
+import organizationRouter from './api-v1/organizationRouter';
+import tournamentRouter from './api-v1/tournamentRouter';
 
 const indexRouter = express.Router();
 
 indexRouter.use('/v1/users/', userRouter);
 indexRouter.use('/v1/organization/', organizationRouter);
-indexRouter.use('/v1/tournament', tournamentRoutes);
+indexRouter.use('/v1/tournament/', tournamentRouter);
 
 export default indexRouter;
