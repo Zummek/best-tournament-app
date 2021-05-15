7679491b277b6a7e0f951fe101e13e7e11ce45ce
eastemployeer <kamilzab27@gmail.com>
2021-05-15 17:32:32 +0200

    Add token proper validation

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -19,10 +19,10 @@ const cookieOptions: CookieOptions = {
   sameSite: 'strict',
 };
 
-const placeTokenInCookie = (token: msal.AuthenticationResult, req: Request, res: Response) => {
+const placeTokenInCookie = (token: string, req: Request, res: Response, tokenName: string) => {
   if (req.secure || req.headers['x-forwarded-proto'] === 'https') { cookieOptions.secure = true; }
 
-  res.cookie('jwt', token.accessToken, cookieOptions);
+  res.cookie(tokenName, token, cookieOptions);
 };
 
 const config = {
@@ -30,6 +30,7 @@ const config = {
     clientId: `${process.env.CLIENT_ID}`,
     authority: `${process.env.AUTHORITY_URL}`,
     clientSecret: `${process.env.CLIENT_SECRET}`,
+    nonce: 123,
   },
   system: {
     loggerOptions: {
@@ -47,8 +48,7 @@ const loggingSession = new msal.ConfidentialClientApplication(config);
 export const login = catchAsync(
   async (req: Request, res: Response) => {
     const authCodeUrlParameters = {
-      scopes: ['user.read', 'user.readbasic.all', 'user.readwrite', 'user.read.all',
-        'user.readwrite.all', 'directory.read.all', 'directory.accessasuser.all'],
+      scopes: ['openid'],
       redirectUri: `${process.env.SSL === 'true' ? 'https' : 'http'}://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}/login`,
     };
 
@@ -66,13 +66,18 @@ export const getToken = catchAsync(
   async (req: Request, res: Response) => {
     const tokenRequest = {
       code: req.body.code as string,
-      scopes: ['user.read'],
+      scopes: ['openid'],
+      nonce: 123,
       redirectUri: 'http://localhost:8080/login',
     };
-
     const token = await loggingSession.acquireTokenByCode(tokenRequest);
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    placeTokenInCookie(token!, req, res);
-    res.status(200).end();
+    if (!token) {
+      return res.status(401).json({
+        message: 'Authentication failed',
+      });
+    }
+    placeTokenInCookie(token.accessToken, req, res, 'jwt');
+    placeTokenInCookie(token.idToken, req, res, 'validation_token');
+    return res.status(200).end();
   },
 );
--- server/middlewares/authenticationMiddleware.ts
+++ server/middlewares/authenticationMiddleware.ts
@@ -1,63 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
-// import { verifyAzureToken } from 'azure-ad-jwt-lite';
-// import jwksClient from 'jwks-rsa';
+import jwksClient from 'jwks-rsa';
+import catchAsync from '../utils/catchAsync';
 
-export default async (req: Request, res: Response, next: NextFunction) => {
-  let token = '';
-
-  if (req.cookies.jwt) {
-    token = req.cookies.jwt;
+export default catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const client = jwksClient({
+    jwksUri: 'https://login.microsoftonline.com/5c17e85e-e180-4b9c-a8ff-5564a210ca4a/discovery/v2.0/keys',
+  });
+  const token = req.cookies.validation_token;
+  const decoded: any = jwt.decode(token, { complete: true });
+  const key = await client.getSigningKey(decoded?.header.kid);
+  const validToken: any = jwt.verify(token, key.getPublicKey());
+  if (validToken) {
+    req.decoded = {
+      userMSId: validToken.oid,
+    };
   } else {
     return res.status(401).json({
       message: 'Authentication failed',
     });
   }
-
-  // TU JEST NAJLEPSZY SPOSOB ALE NO AAAAAAAAA MS SPAPRAŁ ROBOTE
-  // const decoded = await verifyAzureToken(token, {
-  //   issuer: `https://sts.windows.net/${process.env.TENANT_ID}/`,
-  //   audience: `api://${process.env.CLIENT_ID}/user.read`,
-  // }) as any;
-  // OPCJA NR 2 lub 1
-  // const decoded = await verifyAzureToken(token) as any;
-
-  // req.decoded = {
-  //   userMSId: decoded.tid as string,
-  // };
-  // next();
-
-  // TUTAJ również poprawna bardziej basic opcja
-  /* const client = jwksClient({
-    jwksUri: `https://login.microsoftonline.com/${process.env.TENANT_ID}/discovery/keys`,
-    // jwksUri: `https://login.microsoftonline.com/${process.env.TENANT_ID}/discovery/v2.0/keys`,
-  });
-  function getKey(header: jwt.JwtHeader, callback: jwt.SigningKeyCallback) {
-    client.getSigningKey(header.kid, (err, key) => {
-      if (err) {
-        return res.status(401).json({
-          message: 'Authentication failed',
-        });
-      }
-
-      const signingKey = key.getPublicKey();
-      console.log('signingKey', signingKey);
-      callback(err, signingKey);
-    });
-  }
-*/
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const decoded = jwt.decode(token) as any;
-
-  if (!decoded) {
-    return res.status(401).json({
-      message: 'Authentication failed',
-    });
-  }
-
-  req.decoded = {
-    userMSId: decoded.oid,
-  };
-
   next();
-};
+});
