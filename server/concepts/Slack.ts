a5fe32c78d4efe1925e26ffe6a319b4dca77db8b
Adam Jędryka <adam.jedryka98@gmail.com>
2021-05-18 23:58:37 +0200

    Make Slack.connection private

--- server/concepts/Slack.ts
+++ server/concepts/Slack.ts
@@ -1,7 +1,7 @@
 import { WebClient } from '@slack/web-api';
 
 export default class Slack {
-  static connection = new WebClient(process.env.SLACK_APP_TOKEN);
+  private static connection = new WebClient(process.env.SLACK_APP_TOKEN);
 
   public static async getUserList() {
     const response = await Slack.connection.users.list();
