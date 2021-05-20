0b2732e14d24909b51739bba974487f59928991b
Adam Jędryka <adam.jedryka98@gmail.com>
2021-05-20 18:07:51 +0200

    Add functions to send messages

--- server/concepts/Slack.ts
+++ server/concepts/Slack.ts
@@ -1,10 +1,55 @@
 import { WebClient } from '@slack/web-api';
+import logger from '../utils/logger';
 
 export default class Slack {
-  private static connection = new WebClient(process.env.SLACK_APP_TOKEN);
+  private static client = new WebClient(process.env.SLACK_APP_TOKEN);
 
   public static async getUserList() {
-    const response = await Slack.connection.users.list();
-    return response.members;
+    try {
+      const response = await Slack.client.users.list();
+      return response.members || [];
+    } catch (error) {
+      logger.error('Error: get user list', error);
+      throw error;
+    }
+  }
+
+  public static async getUsersChannels(emails: string[]) {
+    try {
+      const users = await Slack.getUserList();
+      const channels: string[] = [];
+
+      emails.forEach((email) => {
+        channels.push(users.find((user) => user.profile?.email === email)?.id || '');
+      });
+
+      return channels;
+    } catch (error) {
+      logger.error('Error: get users channels', error);
+      throw error;
+    }
+  }
+
+  public static async sendMessage(channel: string, message: string) {
+    try {
+      const response = await Slack.client.chat.postMessage({
+        channel,
+        text: message,
+      });
+
+      return response.ok;
+    } catch (error) {
+      if (error.data.error === 'channel_not_found')
+        return false;
+
+      logger.error('Error: send message', error);
+      throw error;
+    }
+  }
+
+  public static async sendMessages(channels: string[], message: string) {
+    channels.forEach(async (channel) => {
+      await Slack.sendMessage(channel, message);
+    });
   }
 }
