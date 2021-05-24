33dc927c6d72784f81a1eb8f7ea551b463100c72
Adam Jędryka <adam.jedryka98@gmail.com>
2021-05-24 22:17:53 +0200

    Add method for open slack conversation and update error's messages in slack class

--- server/concepts/Slack.ts
+++ server/concepts/Slack.ts
@@ -9,47 +9,57 @@ export default class Slack {
       const response = await Slack.client.users.list();
       return response.members || [];
     } catch (error) {
-      logger.error('Error: get user list', error);
+      logger.error('Could not get slack\'s user list', error);
       throw error;
     }
   }
 
-  public static async getUsersChannels(emails: string[]) {
+  public static async getUsersIds(emails: string[]) {
     try {
-      const users = await Slack.getUserList();
-      const channels: string[] = [];
+      const allUsers = await Slack.getUserList();
+      const usersIds: string[] = [];
 
       emails.forEach((email) => {
-        channels.push(users.find((user) => user.profile?.email === email)?.id || '');
+        const userId = allUsers.find((user) => user.profile?.email === email)?.id;
+        if (userId)
+          usersIds.push(userId);
       });
 
-      return channels;
+      return usersIds;
     } catch (error) {
-      logger.error('Error: get users channels', error);
+      logger.error('Could not get slack\'s users ids', error);
       throw error;
     }
   }
 
-  public static async sendMessage(channel: string, message: string) {
+  public static async openConversation(userIds: string[]) {
+    const userIdsString = userIds.join(',');
+    try {
+      const conversationRes = await Slack.client.conversations.open({
+        users: userIdsString,
+      });
+
+      if (!conversationRes.ok || !conversationRes.channel?.id)
+        throw new Error('Could not open slack conversation');
+
+      return conversationRes.channel.id;
+    } catch (error) {
+      logger.error('Could not open slack conversation', error);
+      throw error;
+    }
+  }
+
+  public static async sendMessage(channelId: string, message: string) {
     try {
       const response = await Slack.client.chat.postMessage({
-        channel,
+        channel: channelId,
         text: message,
       });
 
       return response.ok;
     } catch (error) {
-      if (error.data.error === 'channel_not_found')
-        return false;
-
-      logger.error('Error: send message', error);
+      logger.error('Could not send slack message', error);
       throw error;
     }
-  }
-
-  public static async sendMessages(channels: string[], message: string) {
-    channels.forEach(async (channel) => {
-      await Slack.sendMessage(channel, message);
-    });
   }
 }
