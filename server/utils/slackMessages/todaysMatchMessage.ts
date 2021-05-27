d46d8d96bf56a8e0f186165e5336b9c4ba7049b3
Adam Jędryka <adam.jedryka98@gmail.com>
2021-05-27 13:32:47 +0200

    Update specific slack's markdown

--- server/utils/slackMessages/todaysMatchMessage.ts
+++ server/utils/slackMessages/todaysMatchMessage.ts
@@ -1,19 +1,19 @@
 import Tournament, { Match } from '../../../shared/types/Tournament';
 
 const todaysMatchMessage = (tournament: Tournament, matches: Match[]): string => {
-  const matchesLines = matches.map((match) => `* ### _${match.teamA?.name}_ VS _${match.teamB?.name}_\n`);
+  const matchesLines = matches.map((match) => `• _${match.teamA?.name}_ VS _${match.teamB?.name}_\n`);
   // eslint-disable-next-line max-len
   const linkToTournamentDetails = `${process.env.SSL ? 'https' : 'http'}://${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}/tournaments/${tournament.id}`;
 
   return `
-# ${tournament.name}
+*${tournament.name}*
 ${matchesLines}
 Dzisiaj planowo odbywa się powyższe mecze!
 
 Na tej konferencji możecie ustalić szczegóły dzisiejszego dnia. 
 Na przykład kto o której godzinie i na którym stanowisku bedzie grał.
 
-[Tutaj znajdziesz szczegóły tego turnieju.](${linkToTournamentDetails})
+<${linkToTournamentDetails}|Tutaj znajdziesz szczegóły tego turnieju.>
 
 Powodzenia!`;
 };
