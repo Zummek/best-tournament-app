19437052f4433a0231131c4e108bfd5fddc22f9d
Kamil Zaborowski <50885549+eastemployeer@users.noreply.github.com>
2021-06-15 13:53:43 +0200

    [Cu-kz52j4] Desktop list view - fix problems with overflow + Loading state spinner  (#48)
    * Add dates for round robin
    
    * Add single elimination dates generation for matches
    
    * Fixed desktop list item
    
    * Added loading spinner after logging in

--- web-app/quasar.conf.js
+++ web-app/quasar.conf.js
@@ -36,7 +36,7 @@ module.exports = configure(function(ctx) {
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
-      'material-icons' // optional, you are not bound to it
+      'material-icons', // optional, you are not bound to it
     ],
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
@@ -67,17 +67,17 @@ module.exports = configure(function(ctx) {
             enforce: 'pre',
             test: /\.(js|vue)$/,
             loader: 'eslint-loader',
-            exclude: /node_modules/
+            exclude: /node_modules/,
           });
         }
-      }
+      },
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
       port: 8080,
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
@@ -99,8 +99,8 @@ module.exports = configure(function(ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify', 'Dialog'],
-      cssAddon: true
+      plugins: ['Notify', 'Dialog', 'Loading'],
+      cssAddon: true,
     },
 
     // animations: 'all', // --- includes all animations
@@ -109,7 +109,7 @@ module.exports = configure(function(ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false
+      pwa: false,
     },
 
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
@@ -129,30 +129,30 @@ module.exports = configure(function(ctx) {
           {
             src: 'icons/icon-128x128.png',
             sizes: '128x128',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-256x256.png',
             sizes: '256x256',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-384x384.png',
             sizes: '384x384',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+            type: 'image/png',
+          },
+        ],
+      },
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
@@ -162,7 +162,7 @@ module.exports = configure(function(ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true
+      hideSplashscreen: true,
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
@@ -183,7 +183,7 @@ module.exports = configure(function(ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'best-tournament-web-app'
+        appId: 'best-tournament-web-app',
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
@@ -192,7 +192,7 @@ module.exports = configure(function(ctx) {
       extendWebpack(/* cfg */) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
-      }
-    }
+      },
+    },
   };
 });
